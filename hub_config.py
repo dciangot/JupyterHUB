cb26f9bb70e87a5d9675a7e9857f02b62ed9dcba
Diego <dciangot@cern.ch>
2020-07-21 11:19:07 +0200

    first commit

--- hub_config.py
+++ hub_config.py
@@ -12,6 +12,47 @@ import os
 
 from subprocess import check_call
 
+
+# 'http://141d9792-dee7-454e-93ef-89ae9ff07adc.k8s.civo.com:8888/hub/oauth_callback'
+#callback = os.environ["OAUTH_CALLBACK_URL"]
+
+#class EnvAuthenticator(GenericOAuthenticator):
+
+#    @gen.coroutine
+#    def pre_spawn_start(self, user, spawner):
+#        auth_state = yield user.get_auth_state()
+#        import pprint
+#        pprint.pprint(auth_state)
+#        if not auth_state:
+#            # user has no auth state
+#            return
+#        # define some environment variables from auth_state
+#        spawner.environment['ACCESS_TOKEN'] = auth_state['access_token']
+#        spawner.environment['REFRESH_TOKEN'] = auth_state['refresh_token']
+#        spawner.environment['USERNAME'] = auth_state['oauth_user']['preferred_username']
+
+##c.JupyterHub.authenticator_class = GitHubEnvAuthenticator
+#c.JupyterHub.authenticator_class = EnvAuthenticator
+#c.GenericOAuthenticator.oauth_callback_url = callback
+
+# PUT IN SECRET
+#c.GenericOAuthenticator.client_id = '8bed0f49-168-b11af06f2916'
+#c.GenericOAuthenticator.client_secret = 'G7yrGjR_qGcLWS44MadMUMj5xA9_bStwDe1r_8_xkeyDg'
+#c.GenericOAuthenticator.authorize_url = 'https://iam-demo.cloud.cnaf.infn.it/authorize'
+#c.GenericOAuthenticator.token_url = 'https://iam-demo.cloud.cnaf.infn.it/token'
+#c.GenericOAuthenticator.userdata_url = 'https://iam-demo.cloud.cnaf.infn.it/userinfo'
+#c.GenericOAuthenticator.scope = ['openid', 'profile', 'email', 'address', 'offline_access']
+#c.GenericOAuthenticator.username_key = "preferred_username"
+
+#c.GenericOAuthenticator.enable_auth_state = True
+#if 'JUPYTERHUB_CRYPT_KEY' not in os.environ:
+#    warnings.warn(
+#        "Need JUPYTERHUB_CRYPT_KEY env for persistent auth_state.\n"
+#        "    export JUPYTERHUB_CRYPT_KEY=$(openssl rand -hex 32)"
+#    )
+#    c.CryptKeeper.keys = [ os.urandom(32) ]
+
+
 c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
 c.KubeSpawner.cmd = ['jupyterhub-singleuser', '--allow-root']
 c.KubeSpawner.image = 'dciangot/spark:test14'
@@ -21,7 +62,7 @@ c.KubeSpawner.profile_list = [
             'display_name': 'Small - 1CPU 2GB',
             'slug': 'training-python',
             'default': True,
-            'kubespawner_override': { 
+            'kubespawner_override': {
                         'cpu_limit': 1.2,
                         'cpu_guarantee': 0.8,
                         'mem_guarantee': '1.8G',
